4756caef804300fd94cbf8541e92f5167a6e9758
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2019-03-25 00:25:24 +0500

    Push path

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,6 +1,6 @@
-import Dashboard from 'layouts/Dashboard/Dashboard'
-import Submission from 'layouts/Submission/Submission'
-import Result from 'layouts/Submission/Result'
+import Dashboard from './layouts/Dashboard/Dashboard'
+import Submission from './layouts/Submission/Submission'
+import Result from './layouts/Submission/Result'
 
 var indexRoutes = [
   { path: '/attempt/:id', name: 'Submission', component: Submission },
