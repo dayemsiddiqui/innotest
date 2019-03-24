34949fcbef5a0fc08e3ce503e0933db8e92ce6e3
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2019-03-25 01:07:16 +0500

    safa

--- client/src/layouts/Dashboard/Dashboard.jsx
+++ client/src/layouts/Dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ import Header from './../../components/Header/Header'
 import Footer from './../../components/Footer/Footer'
 import Sidebar from './../../components/Sidebar/Sidebar'
 
-import dashboardRoutes from 'routes/dashboard.jsx'
+import dashboardRoutes from './../../routes/dashboard.jsx'
 
 class Dashboard extends Component {
   componentDidUpdate (e) {
